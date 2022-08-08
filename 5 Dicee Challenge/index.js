d5a67da4e2c9fcc2f2de60f1b519a0474c03fb55
LilyV <88788026+5Owls@users.noreply.github.com>
2022-08-08 21:45:05 +0200

    Updated the H1 element to say who won

--- 5 Dicee Challenge/index.js
+++ 5 Dicee Challenge/index.js
@@ -1,7 +1,20 @@
 //Get a random number between 1 and 6
-var playerOneRandomNumber = Math.floor(Math.random()*6);
-var playerTwoRandomNumber = Math.floor(Math.random()*6);
+var playerOneRandomNumber = Math.floor(Math.random()*6) + 1;
+var playerTwoRandomNumber = Math.floor(Math.random()*6) + 1;
+
+//for testing purposes
 console.log(playerOneRandomNumber);
+console.log(playerTwoRandomNumber);
+
+//updates the h1 to say who won
+if (playerOneRandomNumber > playerTwoRandomNumber){
+   document.querySelector("h1").textContent = "🚩Player One Wins!";
+}else if (playerTwoRandomNumber > playerOneRandomNumber) {
+   document.querySelector("h1").textContent = "Player Two Wins!🚩";
+}else{
+  document.querySelector("h1").textContent = "Draw!"
+}
+
 //link images to number...lets use if:
 if(playerOneRandomNumber === 1){
   document.querySelector(".img1").src = "images/dice1.png";
