587956932eb236f42290d4b6fcdcf501ee8a5412
LilyV <88788026+5Owls@users.noreply.github.com>
2022-08-12 16:11:55 +0200

    Updated index.js document
     Updated to better coding practices(using less code by using functions better) and adding animations to buttons.

--- 6 Drum Kit/index.js
+++ 6 Drum Kit/index.js
@@ -1,32 +1,40 @@
-//Old school way of doing it
-// document.querySelectorAll("button")[0].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[1].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[2].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[3].addEventListener("click", handleClick);
-// document.querySelectorAll("button")[4].addEventListener("click", handleClick);
+//Updated version using less code. The previous version used switch statements
+//in two different functions to handle keypress or button clicked.
+//This version combines the two to only use one switch statment but there are
+//still two different functions to handle differnet events.
 
 
+//We get a max value for loop to run through, that doesnt include other buttons.
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+//now we add the event(button clicked with mouse)listeners everytime page refreshes
 for (var i = 0; i < numberOfDrumButtons; i++){
-  //now we add the event(button clicked with mouse)listeners everytime page refreshes
+
   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick );
 }
 
 document.addEventListener("keypress", handleButtonPressed);
 
-function handleButtonPressed(event){
-  //the function that handles webpage if keyboard was whatButtonPressed
 
+//handle click of mouse on page
+function handleClick(){
+  var text = this.innerHTML;
+  console.log(text);
+  makeSound(text);
 
-  //testing event.key functionality
-  // var whatButtonPressed = event.key;
-  // alert("button pressed: " + whatButtonPressed)
+  addAnimation(text);
 
+}
 
-  var whatButtonPressed = event.key;
+//handle click of keyboard
+function handleButtonPressed(){
+  makeSound(event.key);
+  addAnimation(event.key)
+}
 
-  switch(whatButtonPressed){
+//function to playsound
+function makeSound(key){
+  switch(key){
     case "w":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
@@ -60,47 +68,12 @@ function handleButtonPressed(event){
   }
 }
 
+function addAnimation(key){
+  //we need to know what button was pressed so we can apply animation
+  var activeButton = document.querySelector("." + key);
+  activeButton.classList.add("pressed");
 
-function handleClick() {
-  //what to do when the user clicks the numberOfDrumButtons
+  setTimeout(function(){
+    activeButton.classList.remove("pressed")}, 100);
 
-
-  // var myAudio = new Audio("sounds/tom-1.mp3");
-  // myAudio.play();
-
-  
-  var buttonClicked = this.innerHTML;
-
-  switch(buttonClicked){
-    case "w":
-      var crash = new Audio("sounds/crash.mp3");
-      crash.play();
-    break;
-    case "a":
-      var kick = new Audio("sounds/kick-bass.mp3");
-      kick.play();
-    break;
-    case "s":
-      var snare = new Audio("sounds/snare.mp3");
-      snare.play();
-    break;
-    case "d":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-    break;
-    case "j":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-    break;
-    case "k":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-    break;
-    case "l":
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-    break;
-    default:
-      console.log("Wrong button pressed");
-  }
 }
